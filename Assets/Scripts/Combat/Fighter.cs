0153bfc8fdcb998251387bbecf6a1ca1ae36bc53
max.kravchenko.dev@gmail.com <Maxim Kravchenko>
2023-08-02 10:10:45 +0300

    Move Within Range

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour {
-        public void Attack(CombatTarget target)
+        
+        [SerializeField] float weaponRage = 2f;
+        Transform target;
+        
+        private void Update() 
         {
-            print("Take that you short, squat peasant!");
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRage;
+            
+            if (target != null && !isInRange)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
         }
     }
 }
