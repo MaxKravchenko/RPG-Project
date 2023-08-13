abd329eabba3d1767cadb8f14eea819543783b6c
MaxKravchenko <max.kravchenko.dev@gmail.com>
2023-08-13 22:37:09 +0300

    Bugs? What Bugs?

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -45,13 +45,20 @@ namespace RPG.Combat
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
                 //this will trigger the Hit() event.
-                GetComponent<Animator>().SetTrigger("attack");
+                TriggerAtack();
                 timeSinceLastAttack = 0;
             }
         }
 
+        private void TriggerAtack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
         void Hit()
         {
+            if (target == null) { return; }
             target.TakeDamage(weaponDamage);
         }
 
@@ -68,8 +75,14 @@ namespace RPG.Combat
 
         public void Cancel()
         {
-            GetComponent<Animator>().SetTrigger("stopAttack");
+            StopAttack();
             target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
         }
     }
 }
