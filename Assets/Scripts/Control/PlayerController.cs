476470017f0dcd426b9ee16af89cda051a6492dd
MaxKravchenko <max.kravchenko.dev@gmail.com>
2023-08-18 17:35:13 +0300

    Disable Control When Dead

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using RPG.Combat;
+using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
 
@@ -7,51 +8,59 @@ namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour {
     
-    private void Update()
-        {
-            if(InteractWithCombat()) return;
-            if(InteractWithMovement()) return;
-        }
-
-        private bool InteractWithCombat()
-        {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-            foreach (RaycastHit hit in hits)
+        Health health;
+        
+        private void Start() 
+            {   
+                health = GetComponent<Health>();
+            }
+        private void Update()
             {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if(target == null) continue;
+                if(health.IsDead()) return;
                 
-                if (!GetComponent<Fighter>().CanAttack(target.gameObject)) continue;
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    GetComponent<Fighter>().Attack(target.gameObject);
-                }
-                return true;
+                if(InteractWithCombat()) return;
+                if(InteractWithMovement()) return;
             }
-            return false;
-        }
 
-        private bool InteractWithMovement()
-        {
-
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
-
-            if (hasHit)
+            private bool InteractWithCombat()
             {
-                if (Input.GetMouseButtonDown(0))
+                RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+                foreach (RaycastHit hit in hits)
                 {
-                    GetComponent<Mover>().StartMoveAction(hit.point);
-                }
-                return true;    
-            }
-            return false;
-        }
+                    CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                    if(target == null) continue;
+                    
+                    if (!GetComponent<Fighter>().CanAttack(target.gameObject)) continue;
 
-        private static Ray GetMouseRay()
-        {
-            return Camera.main.ScreenPointToRay(Input.mousePosition);
-        }
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        GetComponent<Fighter>().Attack(target.gameObject);
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            private bool InteractWithMovement()
+            {
+
+                RaycastHit hit;
+                bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+
+                if (hasHit)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        GetComponent<Mover>().StartMoveAction(hit.point);
+                    }
+                    return true;    
+                }
+                return false;
+            }
+
+            private static Ray GetMouseRay()
+            {
+                return Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
     }
 }
