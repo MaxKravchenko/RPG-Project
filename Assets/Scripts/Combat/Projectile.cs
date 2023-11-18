2646509ccaf0867b8b4a6f7330a3fefe31c85662
MaxKravchenko <max.kravchenko.dev@gmail.com>
2023-11-19 00:16:37 +0200

    Improve Projectile Behaviour

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -8,14 +8,23 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = true;
         Health target = null;
         float damage = 0;
 
+        private void Start() 
+        {
+            transform.LookAt(GetAimLocation());
+        }
+        
         void Update()
         {
             if (target == null) return;
 
-            transform.LookAt(GetAimLocation());
+            if(isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -38,6 +47,7 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other) 
         {
             if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
